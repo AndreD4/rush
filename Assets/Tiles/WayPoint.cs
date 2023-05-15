10a282a5689fe8136fea4c428bde300e434187ee
AndreD4 <andremai629@gmail.com>
2023-05-15 14:01:47 +0900

    console log tile that mouse is over

--- Assets/Tiles/WayPoint.cs
+++ Assets/Tiles/WayPoint.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class WayPoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  void OnMouseOver() 
+  {
+    Debug.Log(transform.name);
+  }
 }
