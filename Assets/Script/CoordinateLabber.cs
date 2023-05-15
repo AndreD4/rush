e0ddb50c62d2220b56a3ccf1e769e538d4420178
AndreD4 <andremai629@gmail.com>
2023-05-15 11:28:50 +0900

    initialization on Awake

--- Assets/Script/CoordinateLabber.cs
+++ Assets/Script/CoordinateLabber.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 
 [ExecuteAlways]
 public class CoordinateLabber : MonoBehaviour
 {
+    TextMeshPro label;
 
+    void Awake() 
+    {
+       label = GetComponent<TextMeshPro>();
+    }
     void Update()
     {
         if(!Application.isPlaying)
